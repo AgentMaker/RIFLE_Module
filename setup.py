819ea72a886a9db0c179b2ca3334dd439a13ea40
zhanghongji <1029550448@qq.com>
2021-02-25 18:13:39 +0800

    Release 0.11.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='paddle-rifle',
+    version='0.11',
+    packages=["paddle_rifle"],
+    url='https://github.com/GT-ZhangAcer/RIFLE_Module',
+    license='MIT',
+    author='zhanghongji',
+    author_email='zhangacer@foxmail.com',
+    description='本项目则为可用于PaddlePaddle的RIFLE优化策略封装版，支持普通API与高阶API，并且只需向训练代码中插入一行代码即可使用RIFLE策略。 '
+)
