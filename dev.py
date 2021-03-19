2b3ec1aba774f45682d0bbaab624950520832ea8
zhanghongji <1029550448@qq.com>
2021-03-19 20:05:29 +0800

    增加一层全链接，测试更方便，acc也更高

--- dev.py
+++ dev.py
@@ -29,7 +29,8 @@ class Net(nn.Layer):
         self.mp = nn.MaxPool2D(2)
         self.conv2 = nn.Conv2D(3, 16, 3)
         self.mp2 = nn.MaxPool2D(2)
-        self.fc = nn.Linear(400, num_classes)
+        self.fc1 = nn.Linear(400, 100)
+        self.fc2 = nn.Linear(100, num_classes)
 
     def forward(self, inputs):
         x = self.conv1(inputs)
@@ -37,12 +38,13 @@ class Net(nn.Layer):
         x = self.conv2(x)
         x = self.mp2(x)
         x = paddle.flatten(x, 1)
-        x = self.fc(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
         return x
 
 
 net = Net(num_classes=10)
-fc_layer = net.fc
+fc_layer = net.fc2
 
 model = paddle.Model(network=net,
                      inputs=paddle.static.InputSpec([1, 28, 28], name="ipt"),
@@ -58,7 +60,7 @@ model.prepare(sgd, loss, acc)
 # 开始训练并传入RIFLE Callback
 model.fit(train_data,
           test_data,
-          batch_size=128,
+          batch_size=32,
           epochs=20,
-          log_freq=10,
+          log_freq=100,
           callbacks=[rifle_cb])
